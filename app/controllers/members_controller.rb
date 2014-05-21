f2251fee3f4a04eb5ea25bcdf8025f03d9a078a5
Jules BURET <jburet@e1r2p11.42.fr>
2014-05-21 13:50:20 +0200

    Ldap 42
    private method for members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,7 @@
 class MembersController < ApplicationController
+	require 'rubygems'
+	require 'net/ldap'
+	
 	def new
 		@member = Member.new
 	end
@@ -6,9 +9,11 @@ class MembersController < ApplicationController
 	def create
 		params.require(:member).permit!
 		member = Member.new( params[:member] )
-		if (member.login != "" && member.password != "")
+		if (from_42_Ldap(member))
 			flash[:notice] = 'Member created'
 			member.save!
+		else
+			flash[:notice] = "You're not in 42."
 		end
 		redirect_to :root
 	end
@@ -19,5 +24,22 @@ class MembersController < ApplicationController
 
 	def show
 		@member = Member.find_by_id( params[:id] )
+	end
+
+	private
+	def from_42_Ldap(member)
+
+		ldap = Net::LDAP.new(
+			:host => "ldap.42.fr",
+			:port => 636,
+			:base => "ou=2013,ou=people,dc=42,dc=fr",
+			:encryption => {:method => :simple_tls},
+			:username => member.login,
+			:password => member.password)
+		if (ldap.bind_as(:base => "ou=2013,ou=people,dc=42,dc=fr", :filter => "(uid=#{member.login})", :password => member.password))
+			return true
+		else
+			return false
+		end
 	end
 end
